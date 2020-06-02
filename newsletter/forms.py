51b30144f9cdb70ef2c3204704dee1f302118c7e
Gary Harney <garyjh126@gmail.com>
2020-06-02 20:40:04 +1000

    Page model and dynamic homepage

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from .models import Join
 
 class JoinForm(forms.ModelForm):
-    email = forms.EmailField(label="", widget=forms.EmailInput(attrs={'placeholder': 'Your Email', 'class': 'form-control'}))
+    email = forms.EmailField(label="", widget=forms.EmailInput(attrs={'placeholder': 'Your email', 'class': 'form-control'}))
     
     class Meta:
         model = Join
@@ -11,7 +11,7 @@ class JoinForm(forms.ModelForm):
          
     def clean_email(self, *args, **kwargs):
         email = self.cleaned_data.get("email")
-        qs = Join.objects.all()
+        qs = Join.objects.filter(email__iexact=email)
         if qs.exists():
             raise forms.ValidationError("This email already exists")
         return email
