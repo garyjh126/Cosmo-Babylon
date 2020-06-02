46fbaae95d7c9032962e92663447ceaaf0ba4db9
Gary Harney <garyjh126@gmail.com>
2020-06-02 12:15:38 +1000

    Added newsletter app to collect email subscriptions

---
+++ newsletter/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+from .models import Join
+
+class JoinForm(forms.ModelForm):
+    email = forms.EmailField(label="", widget=forms.EmailInput(attrs={'placeholder': 'Your Email', 'class': 'form-control'}))
+    
+    class Meta:
+        model = Join
+        fields = ['email']
+         
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get("email")
+        qs = Join.objects.all()
+        if qs.exists():
+            raise forms.ValidationError("This email already exists")
+        return email
