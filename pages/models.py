aecb32ea9873a7f25c23d0337ccf1875480b380d
Gary Harney <garyjh126@gmail.com>
2020-06-04 15:18:40 +1000

    Update model for page layout

--- pages/models.py
+++ pages/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
+
+LAYOUT_CHOICES = (
+    ('standard', 'Standard'),
+    ('stacked', 'Stacked'),
+)
 class Page(models.Model):
     title                       = models.CharField(max_length=220)
     title_description           = models.TextField(blank=True, null=True)
     title_button                = models.CharField(max_length=50, default="Join")
     title_btn_url               = models.CharField(max_length=50, blank=True, null=True)
     content                     = models.TextField(blank=True, null=True)
+    show_nav                    = models.BooleanField(default=True)
+    nav_color                   = models.CharField(max_length=7, default='#000000')
+    layout                      = models.CharField(max_length=20, choices=LAYOUT_CHOICES, default='standard')
 
     def __str__(self):
         return self.title
