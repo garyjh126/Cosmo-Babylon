5c0dba21ea5582f11a45f3e0c8a7c8b41b00df58
Gary Harney <garyjh126@gmail.com>
2020-06-19 12:13:33 +1000

    Created contacts app

--- newsletter/api/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import generics
-from .serializers import JoinSerializer
-from newsletter.models import Join
-
-class JoinCreateAPIView(generics.CreateAPIView): 
-    queryset = Join.objects.all()
-    serializer_class = JoinSerializer
-    permission_classes = []
-    authentication_classes = []
