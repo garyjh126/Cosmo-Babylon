76fec09d3cfe66032b38ff60cf580f6c6f47be7a
Gary Harney <garyjh126@gmail.com>
2020-06-22 13:43:10 +1000

    key

--- landing_page/settings/production.py
+++ landing_page/settings/production.py
@@ -90,7 +90,6 @@ WSGI_APPLICATION = 'landing_page.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-SECRET_KEY = get_secret('DB_PASSWORD')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
