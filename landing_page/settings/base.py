47357b1a1f019410bc8f4ee9deb89d4973f28795
Gary Harney <garyjh126@gmail.com>
2020-06-22 13:46:33 +1000

    changed base settings for production

--- landing_page/settings/base.py
+++ landing_page/settings/base.py
@@ -20,15 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
 
 
-with open(os.path.join(BASE_DIR, 'secrets.json')) as secrets_file:
-    secrets = json.load(secrets_file)
 
-def get_secret(setting, secrets=secrets):
-    """Get secret setting or fail with ImproperlyConfigured"""
-    try:
-        return secrets[setting]
-    except KeyError:
-        raise ImproperlyConfigured("Set the {} setting".format(setting))
+SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # Quick-start development settings - unsuitable for production
